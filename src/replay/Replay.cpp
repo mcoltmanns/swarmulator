dadd690f7aa3674caf03180476560343cdcb0d4b
Max Oltmanns <moltma@pm.me>
2025-12-05 10:23:28 +0100

    replay initial commit

---
+++ src/replay/Replay.cpp
@@ -0,0 +1,7 @@
+//
+// Created by moltmanns on 12/5/25.
+//
+
+#include "Replay.h"
+
+namespace swarmulator {} // namespace swarmulator
---
+++ src/replay/Replay.h
@@ -0,0 +1,14 @@
+//
+// Created by moltmanns on 12/5/25.
+//
+
+#ifndef SWARMULATOR_CPP_REPLAY_H
+#define SWARMULATOR_CPP_REPLAY_H
+
+namespace swarmulator {
+
+    class Replay {};
+
+} // namespace swarmulator
+
+#endif // SWARMULATOR_CPP_REPLAY_H
