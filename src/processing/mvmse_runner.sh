f6e0a056364f21a34f71f53d64af945f51afdeda
Max Oltmanns <moltma@pm.me>
2026-01-30 08:44:58 +0100

    shell scripts for mvmse and observer processing

--- src/processing/mvmse_runner.sh
+++ src/processing/mvmse_runner.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
 # do one run with sample on all artifact widths - check if different widths give different results (maybe they capture more detail?)
-for a in {3..7}
-do
-    python multiscale_entropy.py /mnt/15799271-aa42-4864-92d2-d1db24402001/pd/pd_1_arts_"$((2 ** a))".h5 /mnt/15799271-aa42-4864-92d2-d1db24402001/pd/pd_1_entropy.h5 width"$((2 ** a))" MvSampEn
-done
-
+# run out of memory for sampen at widths >= 32
 #for a in {3..7}
 #do
-#    for i in {1..5}
-#    do
-#        echo Forage run "$i", width "$((2 ** a))"
-#        python multiscale_entropy.py /mnt/15799271-aa42-4864-92d2-d1db24402001/forage/forage_"$i"_arts_"$((2 ** a))".h5 /mnt/15799271-aa42-4864-92d2-d1db24402001/forage/forage_"$i"_entropy.h5 width"$((2 ** a))"
-#
-#        echo PD run "$i", width "$((2 ** a))"
-#        python multiscale_entropy.py /mnt/15799271-aa42-4864-92d2-d1db24402001/pd/pd_"$i"_arts_"$((2 ** a))".h5 /mnt/15799271-aa42-4864-92d2-d1db24402001/pd/pd_"$i"_entropy.h5 width"$((2 ** a))"
-#
-#        echo pred prey run "$i", width "$((2 ** a))"
-#        python multiscale_entropy.py /mnt/15799271-aa42-4864-92d2-d1db24402001/pred-prey/pred_prey_"$i"_arts_"$((2 ** a))".h5 /mnt/15799271-aa42-4864-92d2-d1db24402001/pred-prey/pred_prey_"$i"_entropy.h5 width"$((2 ** a))"
-#    done
+#    echo PD run 1 width "$((2 ** a))"
+#    python multiscale_entropy.py /mnt/15799271-aa42-4864-92d2-d1db24402001/pd/pd_1_arts_"$((2 ** a))".h5 /mnt/15799271-aa42-4864-92d2-d1db24402001/pd/pd_1_entropy.h5 width"$((2 ** a))" MvSampEn
+#    python multiscale_entropy.py /mnt/15799271-aa42-4864-92d2-d1db24402001/pd/pd_1_arts_"$((2 ** a))".h5 /mnt/15799271-aa42-4864-92d2-d1db24402001/pd/pd_1_entropy.h5 width"$((2 ** a))" MvPermEn
 #done
+
+for a in {3..4}
+do
+    for i in {1..5}
+    do
+        echo Forage run "$i", width "$((2 ** a))"
+        python multiscale_entropy.py /mnt/15799271-aa42-4864-92d2-d1db24402001/forage/forage_"$i"_arts_"$((2 ** a))".h5 /mnt/15799271-aa42-4864-92d2-d1db24402001/forage/forage_"$i"_entropy.h5 width"$((2 ** a))" MvSampEn
+
+        echo PD run "$i", width "$((2 ** a))"
+        python multiscale_entropy.py /mnt/15799271-aa42-4864-92d2-d1db24402001/pd/pd_"$i"_arts_"$((2 ** a))".h5 /mnt/15799271-aa42-4864-92d2-d1db24402001/pd/pd_"$i"_entropy.h5 width"$((2 ** a))" MvSampEn
+
+        echo pred prey run "$i", width "$((2 ** a))"
+        python multiscale_entropy.py /mnt/15799271-aa42-4864-92d2-d1db24402001/pred-prey/pred_prey_"$i"_arts_"$((2 ** a))".h5 /mnt/15799271-aa42-4864-92d2-d1db24402001/pred-prey/pred_prey_"$i"_entropy.h5 width"$((2 ** a))" MvSampEn
+    done
+done
--- src/processing/observer_runner.sh
+++ src/processing/observer_runner.sh
@@ -9,13 +9,13 @@ do
     for i in {1..5}
     do
         echo Forage run "$i", width "$((2 ** a))"
-        python observer_runner.py /mnt/15799271-aa42-4864-92d2-d1db24402001/forage/forage_"$i"_arts_"$((2 ** a))".h5 /mnt/15799271-aa42-4864-92d2-d1db24402001/forage/forage_"$i"_observer_decay_w"$((2 ** a))".h5 128 15 16 512
+        python observer_runner.py /mnt/15799271-aa42-4864-92d2-d1db24402001/forage/forage_"$i"_arts_"$((2 ** a))".h5 /mnt/15799271-aa42-4864-92d2-d1db24402001/forage/forage_"$i"_observer_avg_w"$((2 ** a))".h5 128 15 16 512
 
         echo PD run "$i", width "$((2 ** a))"
-        python observer_runner.py /mnt/15799271-aa42-4864-92d2-d1db24402001/pd/pd_"$i"_arts_"$((2 ** a))".h5 /mnt/15799271-aa42-4864-92d2-d1db24402001/pd/pd_"$i"_observer_decay_w"$((2 ** a))".h5 128 15 16 512
+        python observer_runner.py /mnt/15799271-aa42-4864-92d2-d1db24402001/pd/pd_"$i"_arts_"$((2 ** a))".h5 /mnt/15799271-aa42-4864-92d2-d1db24402001/pd/pd_"$i"_observer_avg_w"$((2 ** a))".h5 128 15 16 512
 
         echo Pred prey run "$i", width "$((2 ** a))"
-        python observer_runner.py /mnt/15799271-aa42-4864-92d2-d1db24402001/pred-prey/pred_prey_"$i"_arts_"$((2 ** a))".h5 /mnt/15799271-aa42-4864-92d2-d1db24402001/pred-prey/pred_prey_"$i"_observer_decay_w"$((2 ** a))".h5 128 15 16 512
+        python observer_runner.py /mnt/15799271-aa42-4864-92d2-d1db24402001/pred-prey/pred_prey_"$i"_arts_"$((2 ** a))".h5 /mnt/15799271-aa42-4864-92d2-d1db24402001/pred-prey/pred_prey_"$i"_observer_avg_w"$((2 ** a))".h5 128 15 16 512
 
     done
 done
